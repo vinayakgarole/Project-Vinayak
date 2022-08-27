ef19e518cd8c768de250bc38d98ac680dfdceda2
vinayakgarole <vinayakgarole10@gmail.com>
2022-08-27 12:47:17 +0530

    added: Single Digit Number

---
+++ IdeaProjects/Day-2-Prictice/src/Digit.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Digit {
+    public static void main(String[] args) {
+        int n, count=0;
+        System.out.println("Enter any Number");
+        Scanner r=new Scanner(System.in);
+        n=r.nextInt();
+        while(n>0)
+        {
+            n=n/10;
+            count++;
+        }
+        System.out.println("No. of digits " +count);
+    }
+}
